a39e4e448ec38f28f3e030b167304ee4436e5633
DuKe786 <71754779+mohitmishra786@users.noreply.github.com>
2021-08-25 09:38:07 +0530

    Add files via upload

---
+++ 1561A.cpp
@@ -0,0 +1,92 @@
+/**
+ *    author:  immadmohit      
+**/
+
+#include <bits/stdc++.h>
+using namespace std;
+#define endl "\n"
+#define ff first
+#define ss second
+#define vec vector
+#define pb push_back
+#define ll long long
+#define vi vector<int>
+#define vll vector<ll>
+#define pll pair<ll,ll>
+#define pi pair<int,int>
+#define mod(a,b) (a%b+b)%b
+#define ceil(a,b) ((a+b-1)/b)
+#define flushout cout.flush();
+#define sz(x) ((int) (x).size())
+#define __lcm(a,b) (a*b)/__gcd(a,b)
+#define all(x) (x).begin(), (x).end()
+#define vin(a) for(auto &ii : a)cin>>ii
+#define f(i,a,b) for(int i=(a);i<(b);i++)
+#define fr(i,a,b) for(int i=(b-1);i>=(a);i--)
+#define dbg(x) cout<<'>'<< #x <<':'<< x <<endl;
+#define pout(x,n) cout<<fixed<<setprecision(x)<<n
+#define vout(v) for(auto ii:v)cout<<ii<<' ';cout<<endl;
+const int M = 1000000007;
+
+/* CODE STARTS HERE */
+
+void init_code(){
+	#ifndef Mohit_Mishra
+    	freopen("Input.txt", "r", stdin);
+    	freopen("Output.txt", "w", stdout);
+    	freopen("Error.txt", "w", stderr);
+    #endif
+}
+
+void solve()
+{
+	int n;
+	cin >> n;
+
+	int arr[n];
+	for(int i = 0 ;i  < n ;i++){
+			cin >> arr[i];
+	}
+
+	int j = 1;
+	int temp;
+	while(!is_sorted(arr,arr + n)){
+		if(j % 2 == 0){
+			for(int i = 0 ; i <  n - 1 ; i++ ){
+				if(((i + 1) % 2 == 0) and (arr[i] > arr[i + 1])){
+					temp  = arr[i];
+					arr[i] = arr[i + 1];
+					arr[i + 1] = temp;
+				}
+			}
+			j++;
+		}
+		else{
+			for(int i = 0 ; i <  n - 1 ; i++ ){
+				if(((i + 1) % 2 != 0) and (arr[i] > arr[i + 1])){
+					temp  = arr[i];
+					arr[i] = arr[i + 1];
+					arr[i + 1] = temp;
+				}
+			}
+			j++;		
+		}
+	}
+	cout << j - 1 << endl;
+
+}
+
+int main(){
+
+	ios_base::sync_with_stdio(false);
+	cin.tie(0);cout.tie(0);
+	init_code();
+	
+	int t;
+	cin >> t;
+	while(t--){
+		
+		solve();
+
+	}	
+}
